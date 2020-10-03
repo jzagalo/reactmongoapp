2357583ddcf6f162e84b64241531cc8bc48e0081
Paul Ezaga <paulezaga@gmail.com>
2020-10-03 02:20:44 +0200

    Transclusion Slots Principles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+function RecentTable(props){
+  return(
+    <table>
+        { props.children }
+    </table>
+  );
+
+}
+
 
 function Heading({ heading }){
   return (
@@ -18,27 +27,31 @@ function Row({ changeSet }){
 }
 
 function Headings({ headings }){
-  return(<tr> 
-    {headings.map((head, index) =>  <Heading  key={index}  heading={head} />)}
-  </tr>)   
+  return(
+    <thead>
+      <tr> 
+        {headings.map((head, index) =>  <Heading  key={index}  heading={head} />)}
+      </tr>
+  </thead>
+  )   
 }
 
 function Rows({ changeSets }){
-  return changeSets.map((row, index) =>  <Row key={index} changeSet={row} />);  
+  return ( 
+      <tbody>
+          { changeSets.map((row, index) =>  <Row key={index} changeSet={row} />) } 
+      </tbody> 
+    );
 }
 
 class App extends  Component { 
  
   render(){          
     return (
-      <table>    
-          <thead>
-            <Headings headings={this.props.headings} />
-          </thead>        
-        <tbody>
-           <Rows changeSets={this.props.data} />
-        </tbody>
-      </table>
+      <RecentTable>           
+         <Headings headings={this.props.headings} />        
+         <Rows changeSets={this.props.data} />        
+      </RecentTable>
     );
 
   }
